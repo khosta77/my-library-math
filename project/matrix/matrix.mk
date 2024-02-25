ae4d20ea875bd075d53d7ba8af5dd131727ed861
khosta77 <puwerfulpants@gmail.com>
2024-02-25 22:23:38 +0300

    Все закоментировал, тк нет cpp то добавить нечего

--- project/matrix/matrix.mk
+++ project/matrix/matrix.mk
@@ -2,20 +2,20 @@
 # Сборщик проекта
 ################################################################################
 
-C_SRCS += \
+#C_SRCS += \
 ./project/main.cpp
 
-C_DEPS += \
+#C_DEPS += \
 ./project/main.d
 
-OBJS += \
+#OBJS += \
 ./project/main.o 
 
-project/%.o: ./project/%.cpp
-	@echo 'Цель построения: $<'
-	@echo 'Операция: Compiler'
-	g++ -Wall -Wextra -Werror -ansi -std=c++17 -O3 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
-	@echo 'Цель готового здания: $<'
-	@echo ' '
+#project/%.o: ./project/%.cpp
+#	@echo 'Цель построения: $<'
+#	@echo 'Операция: Compiler'
+#	g++ -Wall -Wextra -Werror -ansi -std=c++17 -O3 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+#	@echo 'Цель готового здания: $<'
+#	@echo ' '
 
 
