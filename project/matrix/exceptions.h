ae7b8544a8ae71d5e7791ce4c1f0b0c3ba61a60e
khosta77 <puwerfulpants@gmail.com>
2024-03-01 14:15:10 +0300

    Обновил исключение

--- project/matrix/exceptions.h
+++ project/matrix/exceptions.h
@@ -5,6 +5,8 @@
 
 #include "matrix.h"
 
+/** @brief Ошибка матрицы, с выводом сообщения
+ * */
 class MatrixException : public std::exception {
 public:
     explicit MatrixException(const std::string &msg) : m_msg(msg) {}
@@ -16,34 +18,42 @@ private:
     std::string m_msg;
 };
 
+/** @brief Ошибка матрицы при чтении из потока
+ * */
 class InvalidMatrixStream : public MatrixException {
 public:
-    InvalidMatrixStream() : MatrixException("Error occurred while reading from stream") {}
+    InvalidMatrixStream() : MatrixException("Произошла ошибка при чтении из потока") {}
 };
 
+/** @brief Ошибка матрицы, выход за пределы матрицы
+ * */
 class OutOfRange : public MatrixException {
 public:
     OutOfRange(size_t i, size_t j, const Matrix &matrix) : MatrixException(
-                "Indexes (" + std::to_string(i) + ", " + std::to_string(j) +
-                ") are out of range. Matrix size is [" +
+                "Индексы (" + std::to_string(i) + ", " + std::to_string(j) +
+                ") выход за границы матрицы. Размер матрицы [" +
                 std::to_string(matrix.getRows()) + ", " + std::to_string(matrix.getCols()) + "]") {}
 };
 
+/** @brief Ошибка матрицы, несовместимости матрицы или не квадратичности матрицы
+ * */
 class DimensionMismatch : public MatrixException {
 public:
     DimensionMismatch(const Matrix &m1, const Matrix &m2) : MatrixException(
-                    "M1[" + std::to_string(m1.getRows()) + ", " + std::to_string(m1.getCols()) + "] and M2[" +
+                    "M1[" + std::to_string(m1.getRows()) + ", " + std::to_string(m1.getCols()) + "] и M2[" +
                     std::to_string(m2.getRows()) + ", " + std::to_string(m2.getCols()) +
-                    "] are not compatible") {}
+                    "] не совместимы") {}
 
     explicit DimensionMismatch(const Matrix &m) : MatrixException(
                             "M[" + std::to_string(m.getRows()) + ", " + std::to_string(m.getCols()) +
-                            "] isn't square matrix") {}
+                            "] это не квадратная матрица") {}
 };
 
+/** @brief Ошибка матрицы, матрицы не сингулярны, то есть detA == 0
+ * */
 class SingularMatrix : public MatrixException {
 public:
-    SingularMatrix() : MatrixException("Singular matrix") {}
+    SingularMatrix() : MatrixException("Сингулярная матрица") {}
 };
 
 
