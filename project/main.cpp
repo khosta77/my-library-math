f39c7f78123652a5968510f5e43191feb8038c0e
khosta77 <puwerfulpants@gmail.com>
2024-03-06 12:35:44 +0300

    Проверка на ошибки метода прогонки

--- project/main.cpp
+++ project/main.cpp
@@ -1,6 +1,10 @@
 #include "./matrix/test/test.h"
 
 int main() {
+    //std::cout << TestMathHighOperation::RunThrough() << std::endl;
+    //std::cout << TestMathHighOperation::RunThrough() << std::endl;
+    //std::cout << TestMathHighOperation::RunThrough() << std::endl;
+    //std::cout << TestMathHighOperation::RunThrough() << std::endl;
     matrixtest::make();
 	return 0;
 }
